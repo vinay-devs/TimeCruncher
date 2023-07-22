fa04747545b49571e50a88092cd35657a5d517f9
Vinay Dev S <vinaydevs@gmail.com>
2023-07-23 00:25:39 +0530

    Adds the drag Features to the todo Item

--- taskmanagement/src/components/TodoItem.jsx
+++ taskmanagement/src/components/TodoItem.jsx
@@ -1,49 +1,70 @@
-import React, { useState } from "react";
+import React from "react";
+import styled from "styled-components";
+import { Button } from "@material-ui/core";
+import { Draggable } from "react-beautiful-dnd";
 
 function TodoItem(props) {
-  const [complete, setComplete] = useState(false);
-  console.log(complete);
+  // console.log(complete);
+
   function removeTask(id) {
     // console.log(props.id);
     props.deleteItem(id);
   }
   function checkBoxHandler() {
-    console.log(complete);
-    updateStatus(props.id, !complete);
-    return complete ? setComplete(false) : setComplete(true);
+    // console.log(complete);
+    updateStatus(props.id, !props.status);
+    // props.completedTask();
   }
   function updateStatus(id, newStatus) {
     // console.log(newStatus);
     props.updateStatus(id, newStatus);
   }
   return (
-    <div>
-      <li
-        style={{
-          textDecoration: complete ? "line-through" : null,
-          listStyle: "none",
-        }}
-      >
-        <input
-          type="checkbox"
-          value={props.status}
-          onClick={() => {
-            // console.log(complete);
-            checkBoxHandler();
-            // console.log(complete);
-          }}
-        />
-        {props.todo}
-      </li>
-      <button
-        onClick={() => {
-          removeTask(props.id);
-        }}
-      >
-        Delete
-      </button>
-    </div>
+    <Draggable draggableId={props.id.toString()} index={props.index}>
+      {(provided, snapshot) => {
+        return (
+          <li
+            style={{
+              textDecoration: props.status ? "line-through" : null,
+              listStyle: "none",
+            }}
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+          >
+            <input
+              type="checkbox"
+              checked={props.status}
+              onChange={() => {
+                // console.log(complete);
+                checkBoxHandler();
+                // console.log(complete);
+              }}
+            />
+            {props.todo}
+
+            <Button
+              size="small"
+              style={{ backgroundColor: "red" }}
+              onClick={() => {
+                removeTask(props.id);
+              }}
+            >
+              Delete
+            </Button>
+          </li>
+        );
+      }}
+    </Draggable>
   );
 }
 
+const Task = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 5px;
+  padding: 2px;
+  border: 2px solid black;
+`;
 export default TodoItem;
--- taskmanagement/src/components/TodoList.jsx
+++ taskmanagement/src/components/TodoList.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import TodoItem from "./TodoItem";
 import { v4 as uuidv4 } from "uuid";
+import styled from "styled-components";
+import { Button } from "@material-ui/core";
+import { TextField } from "@mui/material";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState("");
-
   console.log(todos);
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -16,7 +19,6 @@ function TodoList() {
       ]);
     }
     setTodo("");
-    // add new todo item to the todos array
   };
   const updateStatus = (id, newStatus) => {
     const updatedItems = todos.map((item) => {
@@ -34,35 +36,123 @@ function TodoList() {
       });
     });
   };
-
+  // const completedTask = () => {
+  //   setCompletedTask([1, 2, 3]);
+  // };
   return (
-    <div>
-      <h1>Todo List</h1>
-      <form onSubmit={handleSubmit}>
-        <input
+    <Content>
+      <H1>Todo List</H1>
+      <Form onSubmit={handleSubmit}>
+        <TextField
+          label="Add Todo"
+          fullWidth
+          variant="outlined"
           type="text"
           onChange={(e) => {
             setTodo(e.target.value);
           }}
           value={todo}
         />
-        <button type="submit">Add</button>
-      </form>
-      <ul>
-        {todos.map((todo, index) => (
-          <TodoItem
-            key={index}
-            id={todo.id}
-            todo={todo.text}
-            deleteItem={deleteItem}
-            status={todo.status}
-            updateStatus={updateStatus}
-          />
-        ))}
-      </ul>
-      <div></div>
-    </div>
+        <Button
+          style={{ backgroundColor: "lightgreen" }}
+          variant="contained"
+          type="submit"
+        >
+          Add
+        </Button>
+      </Form>
+      <Tasks>
+        <Task>
+          <H2>In Progress</H2>
+          <DragDropContext draggableId="drag">
+            <Droppable droppableId="todolist">
+              {(provided) => (
+                <ul
+                  style={{ padding: "0px" }}
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                >
+                  {todos
+                    .filter((item) => !item.status)
+                    .map((todo, index) => {
+                      return (
+                        <TodoItem
+                          key={todo.id}
+                          id={todo.id}
+                          todo={todo.text}
+                          deleteItem={deleteItem}
+                          status={todo.status}
+                          updateStatus={updateStatus}
+                          index={index}
+                          // completedTask={completedTask}
+                        />
+                      );
+                    })}
+                </ul>
+              )}
+            </Droppable>
+          </DragDropContext>
+        </Task>
+        <Task>
+          <H2>Completed</H2>
+          <ul style={{ padding: "0px" }}>
+            {todos
+              .filter((item) => item.status)
+              .map((todo, index) => {
+                // console.log(todo);
+                return (
+                  <TodoItem
+                    key={index}
+                    id={todo.id}
+                    todo={todo.text}
+                    deleteItem={deleteItem}
+                    status={todo.status}
+                    updateStatus={updateStatus}
+                    // completedTask={completedTask}
+                  />
+                );
+              })}
+          </ul>
+        </Task>
+      </Tasks>
+    </Content>
   );
 }
 
+const Task = styled.div`
+  width: 35%;
+  min-height: 300px;
+  height: 40%;
+  border-radius: 7px;
+  border: 2px solid black;
+`;
+const Tasks = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const H1 = styled.h1`
+  text-align: center;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 13px;
+`;
+const Content = styled.div`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  flex-direction: column;
+  row-gap: 25px;
+`;
+
+const H2 = styled.h2`
+  text-align: center;
+  margin: 1px;
+  border: 2px solid black;
+  border-radius: 7px;
+`;
 export default TodoList;
